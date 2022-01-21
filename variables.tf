fdb87ecacfcccbc104538063a29a82fd76497f7d
amirreza <amirrezawh@gmail.com>
2022-01-21 13:00:31 +0330

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "MAAS_API_URL" {
-  default = "http://192.168.56.3:5240/MAAS"
+  default = "<URL>"
 }
 
 variable "MAAS_API_KEY" {
-  default = "9SYWXGJL4UH66qSdhj:yNexbCNMr7q5qY7CGv:ELqabK5tNDCdcyc9UZf7PqAkvkeNUVGt"
+  default = "<API-KEY>"
 }
